e6add98be6bb7592816d569efe4253cfe13b55dd
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-13 16:04:22 +0200

    durch

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+
+
+// let score = 5 + 5 * 10;
+// console.log('Ergebnis: ' + score);
+
+// let score = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score);
+
+
+let score = 0;
+score = score + 10;
+console.log('Ergebnis: ' + score);
+
+
+score += 10;
+console.log('Ergebnis: ' + score);
+
+//Inkrementierung und Dekrementierungen können wie folgt verwendet werden:
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+//Verwende Inkrement-Operatoren
+zahl++;
+console.log('increment: ' + zahl);
+
+//Verwende Dekrement-Operatoren
+zahl--;
+console.log('dekrement: ' + zahl);
+
